1b600d8b7544193a3602371da338a10277b8edf4
Laurie Roy <laurieroy.dev@gmail.com>
2018-10-20 22:40:57 -0700

    fail test 6. canAddDiscountRules

--- TestCheckout.py
+++ TestCheckout.py
@@ -7,14 +7,19 @@ def checkout():
     return checkout
 
 
-def test_CanCalculateTotal(checkout):
+def test_canCalculateTotal(checkout):
     checkout.addItemPrice("a", 1)
     checkout.addItem("a")
     assert checkout.calculateTotal() == 1
+    
 
-def test_GetCorrectTotalWithMultipleItems(checkout):
+def test_getCorrectTotalWithMultipleItems(checkout):
     checkout.addItemPrice("a", 1)
     checkout.addItemPrice("b", 2)
     checkout.addItem("a")
     checkout.addItem("b")
     assert checkout.calculateTotal() == 3
+
+
+def test_canAddDiscountRule(checkout):
+    checkout.addDiscount("a", 3, 2)

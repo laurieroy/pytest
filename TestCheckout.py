da2aebd0d2ae0c813805a15227aa58edd031d416
Laurie Roy <laurieroy.dev@gmail.com>
2018-10-20 22:47:20 -0700

    refactor test 6. addItemPrice a,b added to checkout text fixture

--- TestCheckout.py
+++ TestCheckout.py
@@ -4,18 +4,18 @@ from Checkout import Checkout
 @pytest.fixture()
 def checkout():
     checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
     return checkout
 
 
 def test_canCalculateTotal(checkout):
-    checkout.addItemPrice("a", 1)
+
     checkout.addItem("a")
     assert checkout.calculateTotal() == 1
 
 
 def test_getCorrectTotalWithMultipleItems(checkout):
-    checkout.addItemPrice("a", 1)
-    checkout.addItemPrice("b", 2)
     checkout.addItem("a")
     checkout.addItem("b")
     assert checkout.calculateTotal() == 3

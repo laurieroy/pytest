e40a5909b3ba2297bd2f6edcf873a348d302306d
Laurie Roy <laurieroy.dev@gmail.com>
2018-10-21 20:53:35 -0700

    refactor test 7; break out calculateItemDiscountedTotal from calculateItemTotal

--- Checkout.py
+++ Checkout.py
@@ -29,16 +29,26 @@ class Checkout:
     def calculateTotal(self):
         total = 0
         for item, count in self.items.items():
-            if item in self.discounts:
-                discount = self. discounts[item]
-                if count >= discount.numberOfItems:
-                    numberOfDiscounts = count/discount.numberOfItems
-                    total += numberOfDiscounts * discount.price
-                    remaining = count % discount.numberOfItems
-                    total += remaining * self.prices[item]
-                else:
-                    total += self.prices[item] * count
-            else:
-                total += self.prices[item] * count
+            total += self.calculateItemTotal(item, count)
         return total
 
+    def calculateItemTotal(self, item, count):
+        total = 0
+        if item in self.discounts:
+            discount = self.discounts[item]
+            if count >= discount.numberOfItems:
+                total += self.calculateItemDiscountedTotal(item, count, discount)
+            else:
+                total += self.prices[item] * count
+        else:
+            total += self.prices[item] * count
+
+        return total
+
+    def calculateItemDiscountedTotal(self, item, count, discount):
+        total = 0
+        numberOfDiscounts = count / discount.numberOfItems
+        total += numberOfDiscounts * discount.price
+        remaining = count % discount.numberOfItems
+        total += remaining * self.prices[item]
+        return total
